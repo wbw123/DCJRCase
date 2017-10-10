aa21540f1125b03f7fdb03c76bf769d626f6327d
ZYK <zyk365636386@163.com>
2017-10-10 20:44:53 +0800

    zyktest11

--- dcjrCase/src/main/java/com/chase/dcjrCase/global/Constants.java
+++ dcjrCase/src/main/java/com/chase/dcjrCase/global/Constants.java
@@ -3,5 +3,5 @@ package com.chase.dcjrCase.global;
 public class Constants {
 
     public static final String HOME_URL = "http://192.168.141.81:8080";
-    private String ZHUYOUKANG1;
+    private String ZHUYOUKANG11;
 }
